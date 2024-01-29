ef6f05f7f70fa17c1105013a662158517fe3afb1
Daiane Marcon <daianemarcon@gmail.com>
2024-01-29 01:49:38 -0300

    exemplo de constante

---
+++ src/constants/ligas.ts
@@ -0,0 +1,34 @@
+export const ligas = {
+    material1: {
+        composicao: {
+            C: {
+                min: 0,
+                max: 1,
+            },
+            Mn: {
+                min: 0,
+                max: 2,
+            },
+            Al: {
+                min: 10,
+                max: 15,
+            },
+        },
+    },
+    material2: {
+        composicao: {
+            C: {
+                min: 0,
+                max: 1,
+            },
+            Mn: {
+                min: 0,
+                max: 2,
+            },
+            Al: {
+                min: 10,
+                max: 15,
+            },
+        },
+    },
+}
