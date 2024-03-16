6f53e8495e9fce1715739a26874d09f8ef40411e
Daiane Marcon <daianemarcon@gmail.com>
2024-03-16 17:16:45 -0300

    feat: tipagem para a constante de materiais

---
+++ src/constants/interfaces.ts
@@ -0,0 +1,9 @@
+import { TMateriaPrima } from '../interfaces/ligas'
+
+export type TLigas = {
+  material1: TMateriaPrima
+  material2: TMateriaPrima
+  material3: TMateriaPrima
+  material4: TMateriaPrima
+  material5: TMateriaPrima
+}
