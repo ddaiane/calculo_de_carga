3af83dd4e4714bf685ccc45211cb6fc7e6f2d26a
Daiane Marcon <daianemarcon@gmail.com>
2024-03-16 17:15:19 -0300

    feat: dados default para contexto global

---
+++ src/defaults/ligas.ts
@@ -0,0 +1,64 @@
+import { TMateriaPrima, TCompositionFinal } from '../interfaces/ligas'
+
+export const materiaPrimaDefault: TMateriaPrima = {
+  composicao: {
+    C: {
+      min: 0,
+      max: 0,
+    },
+    Mn: {
+      min: 0,
+      max: 0,
+    },
+    Al: {
+      min: 0,
+      max: 0,
+    },
+    Si: {
+      min: 0,
+      max: 0,
+    },
+    Cr: {
+      min: 0,
+      max: 0,
+    },
+    Ni: {
+      min: 0,
+      max: 0,
+    },
+    Cu: {
+      min: 0,
+      max: 0,
+    },
+    Mo: {
+      min: 0,
+      max: 0,
+    },
+    Nb: {
+      min: 0,
+      max: 0,
+    },
+    W: {
+      min: 0,
+      max: 0,
+    },
+    V: {
+      min: 0,
+      max: 0,
+    },
+  },
+}
+
+export const composicaoResultanteInicial: TCompositionFinal = {
+  C: 0,
+  Mn: 0,
+  Si: 0,
+  Cr: 0,
+  Ni: 0,
+  Cu: 0,
+  Mo: 0,
+  Nb: 0,
+  W: 0,
+  V: 0,
+  Al: 0,
+}
