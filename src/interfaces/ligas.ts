6b77a26a52b7b22baccb298b617b4e1557907935
Daiane Marcon <daianemarcon@gmail.com>
2024-03-16 17:16:19 -0300

    feat: interface de todos tipos de liga

--- src/interfaces/ligas.ts
+++ src/interfaces/ligas.ts
@@ -1,10 +1,38 @@
-export interface IFaixa {
-  min: number
-  max: number
+export type TElementRange = {
+  min: Number
+  max: Number
 }
 
-export interface IComposicao {
-  C: IFaixa | number
-  Mn: IFaixa | number
-  Al: IFaixa | number
+export type TCompositionRange = {
+  C: TElementRange
+  Mn: TElementRange
+  Si: TElementRange
+  Cr: TElementRange
+  Ni: TElementRange
+  Cu: TElementRange
+  Mo: TElementRange
+  Nb: TElementRange
+  W: TElementRange
+  V: TElementRange
+  Al: TElementRange
 }
+
+export type TCompositionFinal = {
+  C: Number
+  Mn: Number
+  Si: Number
+  Cr: Number
+  Ni: Number
+  Cu: Number
+  Mo: Number
+  Nb: Number
+  W: Number
+  V: Number
+  Al: Number
+}
+
+export type TMateriaPrima = {
+  composicao: TCompositionRange
+}
+
+
