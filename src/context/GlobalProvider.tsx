9d7e6ea12b45c4f58925aee434c3c47b8de43b99
Daiane Marcon <daianemarcon@gmail.com>
2024-03-16 17:14:25 -0300

    feat: provider de contexto global

--- src/context/GlobalProvider.tsx
+++ src/context/GlobalProvider.tsx
@@ -1,9 +1,32 @@
-import React, { createContext, useState } from 'react'
+import { createContext, useState } from 'react'
 
-export const GlobalContext = createContext({})
+import { IGlobalContext } from './interfaces'
+import { materiaPrimaDefault, composicaoResultanteInicial } from '../defaults/ligas'
+import { ligas } from '../constants/ligas'
 
-export default function GlobalProvider({ children }: { children: React.ReactNode }) {
-  const context = {}
+export const GlobalContext = createContext({} as IGlobalContext)
+
+export const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
+  const [ligaDesejadaComposicao, setLigaDesejadaComposicao] = useState(
+    materiaPrimaDefault.composicao
+  )
+  const [ligaResultanteComposicao, setLigaResultanteComposicao] = useState(
+    composicaoResultanteInicial
+  )
+  const [ligaDesejadaName, setLigaDesejadaName] = useState('')
+  const [pesoFinalDesejado, setPesoFinalDesejado] = useState(0)
+
+  const context: IGlobalContext = {
+    ligaDesejadaComposicao,
+    setLigaDesejadaComposicao,
+    ligaDesejadaName,
+    setLigaDesejadaName,
+    pesoFinalDesejado,
+    setPesoFinalDesejado,
+    ligaResultanteComposicao,
+    setLigaResultanteComposicao,
+    ligas,
+  }
 
   return <GlobalContext.Provider value={context}>{children}</GlobalContext.Provider>
 }
